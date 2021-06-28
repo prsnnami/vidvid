828b2c05f1d6c55966540a4ef99a5705eb205bc1
Prasanna Mishra <prasannamishra50@gmail.com>
2021-06-28 12:46:54 +0545

    redirect to reels on submit

--- client/src/pages/Editor.jsx
+++ client/src/pages/Editor.jsx
@@ -106,7 +106,7 @@ export default function Editor() {
       body: JSON.stringify(body),
     }).then(res => {
       setExportLoading(false);
-      // navigate('/reels');
+      navigate('/reels');
     });
   }
 
